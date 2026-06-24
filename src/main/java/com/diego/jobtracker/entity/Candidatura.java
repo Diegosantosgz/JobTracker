2039d44dc32ce420df44300cf4de8422e831fd7b
Diego Santos <diegosantosgonzalez7@gmail.com>
2026-06-24 16:26:34 +0200

    Añadido estado a la entidad Candidatura

--- src/main/java/com/diego/jobtracker/entity/Candidatura.java
+++ src/main/java/com/diego/jobtracker/entity/Candidatura.java
@@ -1,12 +1,17 @@
 package com.diego.jobtracker.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import com.diego.jobtracker.enums.EstadoCandidatura;
 
 import java.time.LocalDate;
 
+// Esta entidad representa una candidatura a un puesto de trabajo en el sistema de seguimiento de candidaturas.
+// PostgreSQL utiliza esta clase para mapear  la tabla
 @Entity
 public class Candidatura {
 
@@ -21,4 +26,7 @@ public class Candidatura {
     private LocalDate fechaCandidatura;
 
     private String notas;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoCandidatura estado;
 }
---
+++ src/main/java/com/diego/jobtracker/enums/EstadoCandidatura.java
@@ -0,0 +1,13 @@
+package com.diego.jobtracker.enums;
+
+
+// Sirve para representar un conjunto cerrado de valores
+// Cada valor del enum representa un estado posible de una candidatura en el proceso de selección de empleo.
+public enum EstadoCandidatura {
+
+    ENVIADA,
+    ENTREVISTA,
+    OFERTA,
+    RECHAZADA
+
+}
